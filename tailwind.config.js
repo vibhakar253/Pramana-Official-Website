7a24128d4bedc3d643cd8d54f8a726c66979c7b1
AKS VIBHAKAR <154590845+vibhakar253@users.noreply.github.com>
2024-05-27 01:02:44 +0530

    Final Commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
+export const content = ["./index.html", "./src/**/*.{js,jsx,css}", "./src/public/assets/Events/*.jpg"];
+export const mode = "jit";
+export const theme = {
+  extend: { 
+    screens:{"md":"800px"}
+  },
+  plugins: {
+    'postcss-import': {},
+    'tailwindcss/nesting': {},
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+
+};
+export const plugins = [];
